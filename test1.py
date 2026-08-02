b482909039216f4284b22dd8837b90ea84fc7a91
mohammad <mohammadkohansal0@gmail.com>
2026-08-02 03:52:22 +0330

    esm va famil jadid baraye branch

--- test1.py
+++ test1.py
@@ -1,7 +1,10 @@
 x1 = "Hello, python World!"
 print(x1)
 dic= {
-"name": "mmad", "age": 20, "family": "zare" 
+"name": "mmad", "age": 20 , "family": "zare" 
 }
 print(dic['name'])
 print(type(dic))
+name = "mmad"
+family = "zare"
+print(name + " " + family)
